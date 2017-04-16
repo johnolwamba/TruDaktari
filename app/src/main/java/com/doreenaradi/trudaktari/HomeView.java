7dc0ba025f9d2400f6309c19587152805baa8c53
Johnstone Ananda <johnolwamba@gmail.com>
2017-04-16 09:55:09 +0300

    first commit

--- app/src/main/java/com/doreenaradi/trudaktari/HomeView.java
+++ app/src/main/java/com/doreenaradi/trudaktari/HomeView.java
@@ -1,5 +1,6 @@
 package com.doreenaradi.trudaktari;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,14 +24,7 @@ public class HomeView extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -67,9 +61,9 @@ public class HomeView extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -80,17 +74,30 @@ public class HomeView extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+     if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_slideshow) {
+        Intent i = new Intent(HomeView.this,HomeView.class);
+         startActivity(i);
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_report) {
 
-        } else if (id == R.id.nav_share) {
+         Intent i = new Intent(HomeView.this,ReportView.class);
+         startActivity(i);
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_scan) {
+
+         Intent i = new Intent(HomeView.this,ReportView.class);
+         startActivity(i);
+
+        } else if (id == R.id.nav_usefulinfo) {
+
+         Intent i = new Intent(HomeView.this,UsefulInfoView.class);
+         startActivity(i);
+
+        } else if (id == R.id.nav_help) {
+
+         Intent i = new Intent(HomeView.this,HelpView.class);
+         startActivity(i);
 
         }
 
--- app/src/main/java/com/doreenaradi/trudaktari/SplashScreenView.java
+++ app/src/main/java/com/doreenaradi/trudaktari/SplashScreenView.java
@@ -1,13 +1,49 @@
 package com.doreenaradi.trudaktari;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenView extends AppCompatActivity {
 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1000;
+    //shared preference used to manage user sessions
+    SharedPreferences sharedpreferences;
+    public static final String USERPREFERENCES = "UserDetails" ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_view);
+
+        //check if user has already logged in
+        sharedpreferences = getSharedPreferences(USERPREFERENCES,
+                Context.MODE_PRIVATE);
+
+        new Handler().postDelayed(new Runnable() {
+            /*
+			 * Showing splash screen with a timer. This will be useful when you
+			 * want to show case your app logo / company*/
+            @Override
+            public void run() {
+                Intent i;
+               i = new Intent(SplashScreenView.this, HomeView.class);
+                    startActivity(i);
+                    finish();
+
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onBackPressed (){
+        System.exit(0);
     }
 }
